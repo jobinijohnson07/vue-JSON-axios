8d53dee00914f8bc150481fdfc6180ced59223c2
jobinijohnson07 <jobinijohnson981@gmail.com>
2019-07-10 15:11:13 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
+  <div>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Vue JS Get Data from API By Axios</h1>
+    <div>
+      <div class="post" v-for="post in posts"  :key="post.id">
+        <h3>Title: {{ post.title }}</h3>
+        <p>Description: {{ post.body }}</p>
+      </div >
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    data() {
+      return {
+         posts: []
+      }
+    },
+    mounted() {
+      var self = this;
+      axios.get('http://jsonplaceholder.typicode.com/posts')
+      .then( function(res){
+        self.posts =  res.data;
+        console.log('Data: ', res.data);
+      })
+      .catch(function(error){
+        console.log('Error:' ,error);
+      })
+    }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+
+.post{
+    background: #ccc;
+    margin-bottom: 20px;
+    padding: 10px 20px;
+}
+
+h3 {
+  text-align: left;
+}
+
+p{
+  text-align: left;
 }
 </style>
